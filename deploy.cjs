fbb96df816d19f75eb2fd1405618f195b688dfad
juliorodriguestrader-oss <juliorodrigues.trader@gmail.com>
2026-04-23 11:10:14 -0300

    Add debug logs to deploy script

---
+++ deploy.cjs
@@ -0,0 +1,31 @@
+require('dotenv').config({ path: '.env.local' });
+
+const FtpDeploy = require('ftp-deploy');
+
+const config = {
+  user: process.env.FTP_USER,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: 21,
+  localRoot: __dirname + '/dist',
+  remoteRoot: process.env.FTP_REMOTE_ROOT || '/public_html',
+  include: ['*', '**/*'],
+  exclude: ['dist/**/*.map', 'node_modules/**', 'src/**', '.git/**', '.vscode/**'],
+  deleteRemote: false,
+  forcePasv: true,
+};
+
+console.log('Deploy config:', {
+  host: config.host,
+  remoteRoot: config.remoteRoot,
+  localRoot: config.localRoot,
+});
+
+const ftpDeploy = new FtpDeploy();
+
+ftpDeploy.deploy(config)
+  .then(() => console.log('Deploy realizado com sucesso!'))
+  .catch(err => {
+    console.error('Erro no deploy:', err);
+    process.exit(1);
+  });
